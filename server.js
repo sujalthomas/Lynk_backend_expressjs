cbad3bbcbfebed0e1f22d1121d8f08860eba3e4d
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-07-24 22:08:19 -0700

    BackEnd Express 4 Testing

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.post("/generate-cover-letter", async (req, res) => {
+  try {
+    console.log(JSON.stringify(req.body));
+
+
+
+    res.status(200).send("Data received and logged.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while receiving the data.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
